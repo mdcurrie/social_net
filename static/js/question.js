750ac97f3a5d45297ad42e10a5bf3459fca73a53
mdcurrie <mcurrie1@umd.edu>
2016-08-03 00:37:20 -0400

    Add files via upload

--- static/js/question.js
+++ static/js/question.js
@@ -1,13 +1,3 @@
 $(function() {
-	var scroll_pos = 0;
-    $(document).scroll(function() { 
-        scroll_pos = $(this).scrollTop();
-        if(scroll_pos >= 80) {
-            $("header").css('background-color', '#eee04b');
-            $("header a").css('color', '#333');
-        } else {
-            $("header").css('background-color', '#333');
-            $("header a").css('color', 'white');
-        }
-    });
+    
 });
