4e27bb019128cd33164dfa38c3f622fd630443f9
mdcurrie <mcurrie1@umd.edu>
2016-09-07 01:08:16 -0400

    Add files via upload

--- static/js/sidecolumn_module.js
+++ static/js/sidecolumn_module.js
@@ -44,4 +44,73 @@ $(function() {
 		}
 		$('#topics form').unbind('submit').submit();
 	});
+
+	$('#question-form-wrapper form').on('submit', function(e) {
+		e.preventDefault();
+		if ($('#question-title').val() == '')
+			return;
+		if ($('#image-link').val() == '')
+			return;
+		if ($('#choice-a').val() == '')
+			return;
+		if ($('#choice-b').val() == '')
+			return;
+		if ($('#topics-input').val() == '')
+			return;
+		if (!(/^[a-zA-Z0-9 \-]+$/i.test($('#topics-input').val()))) {
+			return;
+		}
+		if ($('#choice-a').val() == $('#choice-b').val())
+			return;
+
+		$("<img>", {
+			src: $('#image-link').val(),
+			error: function() {
+				;
+			},
+			load: function() {
+				$('#question-form-wrapper form').unbind('submit').submit();
+			}
+		});
+	});
+
+	$('#choice-c').on('input', function() {
+		if ($('#choice-c').val() != '') {
+			if ($('.input-section:nth-of-type(6)').css("display") == "none") {
+				$('.input-section:nth-of-type(6)').css({"display": "block"});
+				$('.input-section:nth-of-type(6)').animate({"opacity": 1}, 300);
+			}
+		}
+		else {
+			if ($('.input-section:nth-of-type(6)').css("display") == "block") {
+				$('#choice-d').val('');
+				$('.input-section:nth-of-type(6)').animate({"opacity": 0}, 300, function() {
+					$('.input-section:nth-of-type(6)').css({"display": "none"});
+				});
+			}
+			if ($('.input-section:nth-of-type(7)').css("display") == "block") {
+				$('#choice-e').val('');
+				$('.input-section:nth-of-type(7)').animate({"opacity": 0}, 300, function() {
+					$('.input-section:nth-of-type(7)').css({"display": "none"});
+				});
+			}
+		}
+	});
+
+	$('#choice-d').on('input', function() {
+		if ($('#choice-d').val() != '') {
+			if ($('.input-section:nth-of-type(7)').css("display") == "none") {
+				$('.input-section:nth-of-type(7)').css({"display": "block"});
+				$('.input-section:nth-of-type(7)').animate({"opacity": 1}, 300);
+			}
+		}
+		else {
+			if ($('.input-section:nth-of-type(7)').css("display") == "block") {
+				$('#choice-e').val('');
+				$('.input-section:nth-of-type(7)').animate({"opacity": 0}, 300, function() {
+					$('.input-section:nth-of-type(7)').css({"display": "none"});
+				});
+			}
+		}
+	});
 });
