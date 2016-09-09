1130b0dfd7f5350c5636946cf210f953bf0f62ad
mdcurrie <mcurrie1@umd.edu>
2016-09-09 00:04:22 -0400

    Add files via upload

--- static/js/settings.js
+++ static/js/settings.js
@@ -10,6 +10,10 @@ $(function() {
 		e.preventDefault();
 		var picture_link = $('#user-profile-pic form input').eq(1).val();
 
+		if (!$('#profile-pic-error').length) {
+			$('#user-profile-pic').append('<div id="profile-pic-error"></div>');
+		}
+
 		$("<img>", {
 			src: picture_link,
 			error: function() {
@@ -17,7 +21,19 @@ $(function() {
 				$('#profile-pic-error').css({"display": "block"});
 			},
 			load: function() {
-				$('#user-profile-pic form').unbind('submit').submit();
+				$.post('/settings/updatePicture', {_xsrf: getCookie('_xsrf'), "profile-picture": picture_link}, function(data) {
+					if (data.error) {
+						$('#profile-pic-error').text(data.error);
+					}
+					else {
+						$('#profile-pic-error').remove();
+						$('#user-profile-pic img').attr({"src": picture_link});
+						$('#current-user-profile-pic img').attr({"src": picture_link});
+						$('#user-profile-pic form').css({"display": "none"});
+						$('input[name="profile-picture"]').attr({"placeholder": picture_link}).val('');
+						$('#user-profile-pic img').css({"opacity": 1});
+					}
+				});
 			}
 		});
 	});
@@ -32,6 +48,10 @@ $(function() {
 	$('#username form').on('submit', function(e) {
 		e.preventDefault();
 		var username = $('#username input').eq(1).val();
+
+		if (!$('#username-error').length) {
+			$('#username').append('<div id="username-error"></div>');
+		}
 
 		if (username == '') {
 			$('#username-error').text("You must enter a username.");
@@ -49,7 +69,18 @@ $(function() {
 			return;
 		}
 
-		$('#username form').unbind('submit').submit();
+		$.post('/settings/updateUsername', {_xsrf: getCookie('_xsrf'), username: username}, function(data) {
+			if (data.error) {
+				$('#username-error').text(data.error);
+			}
+			else {
+				$('#username-error').remove();
+				$('#username .wrapper').text(username);
+				$('#username form').css({"display": "none"});
+				$('input[name="username"]').attr({"placeholder": username}).val('');
+				$('#username .wrapper').css({"display": "inline-block"});
+			}
+		});
 	});
 
 	/* bio */
@@ -57,6 +88,18 @@ $(function() {
 		$(this).css({"display": "none"});
 		$('#bio form').css({"display": "inline-block"});
 		$('#bio input').focus();
+	});
+
+	$('#bio form').on('submit', function(e) {
+		e.preventDefault();
+		var bio = $('#bio input').eq(1).val();
+
+		$.post('/settings/updateBio', {_xsrf: getCookie('_xsrf'), bio: bio}, function(data) {
+			$('#bio .wrapper').text(bio);
+			$('#bio form').css({"display": "none"});
+			$('input[name="bio"]').attr({"placeholder": bio.slice(0, 140)}).val('');
+			$('#bio .wrapper').css({"display": "inline-block"});
+		});
 	});
 
 	/* email */
@@ -70,13 +113,27 @@ $(function() {
 		e.preventDefault();
 		var email = $('.row:first-of-type input').eq(1).val();
 
+		if (!$('#email-error').length) {
+			$('.row:first-of-type').append('<div class="error-message" id="email-error"></div>');
+		}
+
 		if (!(/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/i.test(email))) {
 			$('#email-error').text('That is not a valid email address.');
-			$('#email-error').css({"display": "block"});
 			return;
 		}
 
-		$('.row:first-of-type form').unbind('submit').submit();
+		$.post('/settings/updateEmail', {_xsrf: getCookie('_xsrf'), email: email}, function(data) {
+			if (data.error) {
+				$('#email-error').text(data.error);
+			}
+			else {
+				$('#email-error').remove();
+				$('#email').text(email);
+				$('.row:first-of-type form').css({"display": "none"});
+				$('input[name="email"]').attr({"placeholder": email}).val('');
+				$('#email').css({"display": "block"});
+			}
+		});
 	});
 
 	/* password */
@@ -91,13 +148,27 @@ $(function() {
 		var old_password = $('.row:nth-of-type(2) input').eq(1).val();
 		var new_password = $('.row:nth-of-type(2) input').eq(2).val();
 
+		if (!$('#reset-password-error').length) {
+			$('.row:nth-of-type(2)').append('<div class="error-message" id="reset-password-error"></div>');
+		}
+
 		if (new_password.length < 6) {
 			$('#reset-password-error').text("Your new password must contain at least 6 characters.");
-			$('#reset-password-error').css({"display": "block"});
 			return;
 		}
 
-		$('.row:nth-of-type(2) form').unbind('submit').submit();
+		$.post('/settings/updatePassword', {_xsrf: getCookie('_xsrf'), "old-password": old_password, "new-password": new_password}, function(data) {
+			if (data.error) {
+				$('#reset-password-error').text(data.error);
+			}
+			else {
+				$('#reset-password-error').remove();
+				$('.row:nth-of-type(2) form').css({"display": "none"});
+				$('input[name="old-password"]').val('');
+				$('input[name="new-password"]').val('');
+				$('#reset-password-button').css({"display": "block"});
+			}
+		});
 	});
 
 	/* custom URL */
@@ -111,22 +182,39 @@ $(function() {
 		e.preventDefault();
 		var custom_url = $('.row:nth-of-type(3) input').eq(1).val();
 
+		if (!$('#custom-url-error').length) {
+			$('.row:nth-of-type(3)').append('<div class="error-message" id="custom-url-error"></div>');
+		}
+
 		if (custom_url.length >= 1 && custom_url.length < 6) {
 			$('#custom-url-error').text("Your custom URL must contain at least 6 characters.");
-			$('#custom-url-error').css({"display": "block"});
 			return;
 		}
 		if (custom_url == "signup" || custom_url == "login" || custom_url == "feed" || custom_url == "settings") {
 			$('#custom-url-error').text("Sorry, but that custom URL is not allowed.");
-			$('#custom-url-error').css({"display": "block"});
 			return;
 		}
-		if (custom_url != '' && !(/^[a-zA-Z0-9_ ]+$/i.test(custom_url))) {
+		if (custom_url != '' && !(/^[a-zA-Z0-9_]+$/i.test(custom_url))) {
 			$('#custom-url-error').text("Your custom URL can only contain letters, numbers, and, underscores.");
-			$('#custom-url-error').css({"display": "block"});
 			return;
 		}
 
-		$('.row:nth-of-type(3) form').unbind('submit').submit();
+		$.post('/settings/updateURL', {_xsrf: getCookie('_xsrf'), "custom-url": custom_url}, function(data) {
+			if (data.error) {
+				$('#custom-url-error').text(data.error);
+			}
+			else {
+				$('#custom-url-error').remove();
+				$('#custom-url').text('https://hive.com/' + custom_url);;
+				$('.row:nth-of-type(3) form').css({"display": "none"});
+				$('input[name="custom-url"]').attr({"placeholder": custom_url}).val('');
+				$('#custom-url').css({"display": "block"});
+			}
+		});
 	});
 });
+
+function getCookie(name) {
+    var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
+    return r ? r[1] : undefined;
+}
