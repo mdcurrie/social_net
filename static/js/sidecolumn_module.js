ec40aa6ebe37d074d701f7fca5e5c669f310bcc8
mdcurrie <mcurrie1@umd.edu>
2016-09-15 00:26:01 -0400

    Add files via upload

--- static/js/sidecolumn_module.js
+++ static/js/sidecolumn_module.js
@@ -58,6 +58,17 @@ $(function() {
 		}
 		$('.input-section:first-of-type .question-form-error').remove();
 
+		if ($('#question-title').val().length > 120) {
+			if (!$('.input-section:first-of-type .question-form-error').length)
+				$('.input-section:first-of-type').append('<div class="question-form-error">Question titles must be 120 characters or less.</div>');
+			else
+				$('.input-section:first-of-type .question-form-error').text('Question titles must be 120 characters or less.');
+			
+			$('#question-title').focus();
+			return;
+		}
+		$('.input-section:first-of-type .question-form-error').remove();
+
 		if ($('#image-link').val() == '') {
 			if (!$('.input-section:nth-of-type(2) .question-form-error').length)
 				$('.input-section:nth-of-type(2)').append('<div class="question-form-error">Please enter a link to an image.</div>');
@@ -91,6 +102,17 @@ $(function() {
 		}
 		$('.input-section:nth-of-type(4) .question-form-error').remove();
 
+		if ($('#choice-a').val().length > 40 || $('#choice-b').val().length > 40 || $('#choice-c').val().length > 40 || $('#choice-d').val().length > 40 || $('#choice-e').val().length > 40) {
+			if (!$('.input-section:nth-of-type(3) .question-form-error').length)
+				$('.input-section:nth-of-type(3)').append('<div class="question-form-error">Choices must be 40 characters or less.</div>');
+			else
+				$('.input-section:nth-of-type(3) .question-form-error').text('Choices must be 40 characters or less.');
+			
+			$('#choice-a').focus();
+			return;
+		}
+		$('.input-section:nth-of-type(3) .question-form-error').remove();
+
 		if ($('#topics-input').val() == '') {
 			if (!$('.input-section:nth-of-type(8) .question-form-error').length)
 				$('.input-section:nth-of-type(8)').append('<div class="question-form-error">Please enter at least 1 topic.</div>');
@@ -114,23 +136,81 @@ $(function() {
 		$('.input-section:nth-of-type(8) .question-form-error').remove();
 
 		if ($('#choice-a').val() == $('#choice-b').val()) {
-			if (!$('.input-section:nth-of-type(4) .question-form-error').length)
-				$('.input-section:nth-of-type(4)').append('<div class="question-form-error">Choice B must be different from Choice A.</div>');
+			if (!$('.input-section:nth-of-type(3) .question-form-error').length)
+				$('.input-section:nth-of-type(3)').append('<div class="question-form-error">Choice A must be different from Choice B.</div>');
 			else
-				$('.input-section:nth-of-type(4) .question-form-error').text('Choice B must be different from Choice A.');
+				$('.input-section:nth-of-type(3) .question-form-error').text('Choice A must be different from Choice B.');
 			
-			$('#choice-b').focus();
+			$('#choice-a').focus();
 			return;
 		}
-		$('.input-section:nth-of-type(4) .question-form-error').remove();
+		$('.input-section:nth-of-type(3) .question-form-error').remove();
 
 		$("<img>", {
 			src: $('#image-link').val(),
 			error: function() {
-				;
+				if (!$('.input-section:nth-of-type(2) .question-form-error').length)
+					$('.input-section:nth-of-type(2)').append('<div class="question-form-error">Please enter a link to an image.</div>');
+				else
+					$('.input-section:nth-of-type(2) .question-form-error').text('Please enter a link to an image.');
+				
+				$('#image-link').focus();
+				return;
 			},
+
 			load: function() {
-				$('#question-form-wrapper form').unbind('submit').submit();
+				$('.input-section:nth-of-type(2) .question-form-error').remove();
+
+				var question_data = $('#question-form-wrapper form').serialize();
+				$.post('/create_question', question_data, function(data) {
+					if (data.title_error) {
+						if (!$('.input-section:first-of-type .question-form-error').length)
+							$('.input-section:first-of-type').append('<div class="question-form-error">' + data.title_error + '</div>');
+						else
+							$('.input-section:first-of-type .question-form-error').text(data.title_error);
+						
+						$('#question-title').focus();
+						return;
+					}
+					$('.input-section:first-of-type .question-form-error').remove();
+
+					if (data.image_error) {
+						if (!$('.input-section:nth-of-type(2) .question-form-error').length)
+							$('.input-section:nth-of-type(2)').append('<div class="question-form-error">' + data.image_error + '</div>');
+						else
+							$('.input-section:nth-of-type(2) .question-form-error').text(data.image_error);
+						
+						$('#image-link').focus();
+						return;
+					}
+					$('.input-section:nth-of-type(2) .question-form-error').remove();
+
+					if (data.choice_error) {
+						if (!$('.input-section:nth-of-type(3) .question-form-error').length)
+							$('.input-section:nth-of-type(3)').append('<div class="question-form-error">' + data.choice_error + '</div>');
+						else
+							$('.input-section:nth-of-type(3) .question-form-error').text(data.choice_error);
+						
+						$('#choice-a').focus();
+						return;
+					}
+					$('.input-section:nth-of-type(3) .question-form-error').remove();
+
+					if (data.topics_error) {
+						if (!$('.input-section:nth-of-type(8) .question-form-error').length)
+							$('.input-section:nth-of-type(8)').append('<div class="question-form-error">' + data.topics_error + '</div>');
+						else
+							$('.input-section:nth-of-type(8) .question-form-error').text(data.topics_error);
+						
+						$('#topics-input').focus();
+						return;
+					}
+					$('.input-section:nth-of-type(8) .question-form-error').remove();
+
+					if (data.question_link) {
+						window.location.href = data.question_link;
+					}
+				});
 			}
 		});
 	});
