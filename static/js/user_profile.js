463d721c9807d4b0180d2132629b793ca2e44ed3
mdcurrie <mcurrie1@umd.edu>
2016-10-16 15:30:49 -0400

    Add files via upload

--- static/js/user_profile.js
+++ static/js/user_profile.js
@@ -1,27 +1,89 @@
 $(function() {
-	$('#followers-count, #following-count').on('click', function() {
+	$('#followers-count').on('click', function() {
 		var url = $(this).attr('class');
 		$.get(url, function(data) {
-			if ($('#relation-wrapper').length) {
-				$('#relation-wrapper').replaceWith(data);
-			}
-			else {
-				$('#misc').prepend(data);
-				$('#relation-wrapper').css({"display": "none"});
-				if ($(window).width() >= 900) {
-					var height = $('#relation-wrapper').height() - 15;
+			if (!$('#follower-wrapper').length) {
+				if (!$('#following-wrapper').length) {
+					$('#misc').append(data);
+					$('.user img').height($('.user img').width());
+					$('#follower-wrapper').height("100%");
+					$('#misc .backer').transition({x: 0}, 300, 'cubic-bezier(1.000, 0.000, 1.000, 1.015)');
+					$('#follower-wrapper').transition({x: 0}, 500, 'cubic-bezier(1.000, 0.000, 0.585, 1.000)');
 				}
 				else {
-					var height = $('#relation-wrapper').height() + 3;
+					$('#misc').append(data);
+					$('.user img').height($('.user img').width());
+					$('#follower-wrapper').height("100%");
+					var z_index = parseInt($('#following-wrapper').css("z-index"));
+					$('#misc .backer').css({"z-index": z_index + 1}).transition({x: "-100%"}, 0).transition({x: 0}, 300, 'cubic-bezier(1.000, 0.000, 1.000, 1.015)');
+					$('#follower-wrapper').css({"z-index": z_index + 2}).transition({x: "-100%"}, 0).transition({x: 0}, 500, 'cubic-bezier(1.000, 0.000, 0.585, 1.000)');
 				}
-				$('#misc').height($('#misc').height() + height);
-				$('.profile-questions').transition({y: height}, 200, function() {
-					$('.profile-questions').transition({y: 0}, 0);
-					$('#relation-wrapper').css({"opacity": 0, "display": "block"}).transition({opacity: 1}, 200);
-					$('.user img').css({"height": $('.user img').eq(0).width() + 'px'});
-				});
+			}
+			else if ($('#following-wrapper').css("z-index") >= $('#follower-wrapper').css("z-index")) {
+				var z_index = parseInt($('#following-wrapper').css("z-index"));
+				$('#misc .backer').css({"z-index": z_index + 1}).transition({x: "-100%"}, 0).transition({x: 0}, 300, 'cubic-bezier(1.000, 0.000, 1.000, 1.015)');
+				$('#follower-wrapper').css({"z-index": z_index + 2}).transition({x: "-100%"}, 0).transition({x: 0}, 500, 'cubic-bezier(1.000, 0.000, 0.585, 1.000)');
+			}
+			else {
+				$('#misc .backer').transition({x: 0}, 300, 'cubic-bezier(1.000, 0.000, 1.000, 1.015)');
+				$('#follower-wrapper').transition({x: 0}, 500, 'cubic-bezier(1.000, 0.000, 0.585, 1.000)');
 			}
 		});
+	});
+
+	$('#following-count').on('click', function() {
+		var url = $(this).attr('class');
+		$.get(url, function(data) {
+			if (!$('#following-wrapper').length) {
+				if (!$('#follower-wrapper').length) {
+					$('#misc').append(data);
+					$('.user img').height($('.user img').width());
+					$('#following-wrapper').height("100%");
+					$('#misc .backer').transition({x: 0}, 300, 'cubic-bezier(1.000, 0.000, 1.000, 1.015)');
+					$('#following-wrapper').transition({x: 0}, 500, 'cubic-bezier(1.000, 0.000, 0.585, 1.000)');
+				}
+				else {
+					$('#misc').append(data);
+					$('.user img').height($('.user img').width());
+					$('#following-wrapper').height("100%");
+					var z_index = parseInt($('#follower-wrapper').css("z-index"));
+					$('#misc .backer').css({"z-index": z_index + 1}).transition({x: "-100%"}, 0).transition({x: 0}, 300, 'cubic-bezier(1.000, 0.000, 1.000, 1.015)');
+					$('#following-wrapper').css({"z-index": z_index + 2}).transition({x: "-100%"}, 0).transition({x: 0}, 500, 'cubic-bezier(1.000, 0.000, 0.585, 1.000)');
+				}
+			}
+			else if ($('#follower-wrapper').css("z-index") >= $('#following-wrapper').css("z-index")) {
+				var z_index = parseInt($('#follower-wrapper').css("z-index"));
+				$('#misc .backer').css({"z-index": z_index + 1}).transition({x: "-100%"}, 0).transition({x: 0}, 300, 'cubic-bezier(1.000, 0.000, 1.000, 1.015)');
+				$('#following-wrapper').css({"z-index": z_index + 2}).transition({x: "-100%"}, 0).transition({x: 0}, 500, 'cubic-bezier(1.000, 0.000, 0.585, 1.000)');
+			}
+			else {
+				$('#misc .backer').transition({x: 0}, 300, 'cubic-bezier(1.000, 0.000, 1.000, 1.015)');
+				$('#following-wrapper').transition({x: 0}, 500, 'cubic-bezier(1.000, 0.000, 0.585, 1.000)');
+			}
+		});
+	});
+
+	$('#question-count').on('click', function() {
+		if ($('#follower-wrapper').length && !$('#following-wrapper').length) {
+			$('#follower-wrapper').transition({x: "-100%"}, 300, 'cubic-bezier(1.000, 0.000, 1.000, 1.015)');
+			$('#misc .backer').transition({x: "-100%"}, 500, 'cubic-bezier(1.000, 0.000, 0.585, 1.000)');
+		}
+		if (!$('#follower-wrapper').length && $('#following-wrapper').length) {
+			$('#following-wrapper').transition({x: "-100%"}, 300, 'cubic-bezier(1.000, 0.000, 1.000, 1.015)');
+			$('#misc .backer').transition({x: "-100%"}, 500, 'cubic-bezier(1.000, 0.000, 0.585, 1.000)');
+		}
+		if ($('#follower-wrapper').length && $('#following-wrapper').length) {
+			if ($('#follower-wrapper').css("z-index") >= $('#following-wrapper').css("z-index")) {
+				$('#following-wrapper').transition({x: "-100%"}, 0);
+				$('#follower-wrapper').transition({x: "-100%"}, 300, 'cubic-bezier(1.000, 0.000, 1.000, 1.015)');
+				$('#misc .backer').transition({x: "-100%"}, 500, 'cubic-bezier(1.000, 0.000, 0.585, 1.000)');
+			}
+			else {
+				$('#follower-wrapper').transition({x: "-100%"}, 0);
+				$('#following-wrapper').transition({x: "-100%"}, 300, 'cubic-bezier(1.000, 0.000, 1.000, 1.015)');
+				$('#misc .backer').transition({x: "-100%"}, 500, 'cubic-bezier(1.000, 0.000, 0.585, 1.000)');
+			}
+		}
 	});
 
 	$('#username-and-button button').on('click', function(e) {
