ce80a7f0c0fe7a998436d9dc287e19736a1beb66
mdcurrie <mcurrie1@umd.edu>
2016-10-21 02:09:48 -0400

    Add files via upload

--- static/js/question.js
+++ static/js/question.js
@@ -1,11 +1,11 @@
 $(function() {
-	if ($(window).width() >= 900) {
+	if (window.innerWidth >= 900) {
 		var new_height = $('.question-wrapper').height() + 'px';
 		$(".comment-section").css({"height": new_height});
 	}
 
 	$(window).resize(function() {
-		if ($(window).width() >= 900) {
+		if (window.innerWidth >= 900) {
 			var new_height = $('.question-wrapper').height() + 'px';
 			$(".comment-section").css({"height": new_height});
 		}
